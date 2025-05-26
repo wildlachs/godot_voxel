11d7a1edb48baf3cdcc6bd86568aeb4fa8b52dd4
wildlachs <kokoskern@gmail.com>
2025-05-26 10:44:27 +0200

    Run code formatter on rgblight.h

--- util/rgblight.h
+++ util/rgblight.h
@@ -3,8 +3,8 @@
 
 #include "modules/voxel/util/math/vector3f.h"
 #include "util/math/vector3f.h"
-#include <array>
 #include <algorithm>
+#include <array>
 #include <cstdint>
 #include <cstdio>
 
@@ -19,18 +19,18 @@ enum class Direction {
 };
 
 struct alignas(1) RGBLight {
-    uint8_t r = 0;
-    uint8_t g = 0;
-    uint8_t b = 0;
+	uint8_t r = 0;
+	uint8_t g = 0;
+	uint8_t b = 0;
 	uint8_t range = 0;
 
-    void dim(uint8_t amount, uint8_t minAmount) {
-        r = (r - amount > minAmount) ? r - amount : minAmount;
-        g = (g - amount > minAmount) ? g - amount : minAmount;
-        b = (b - amount > minAmount) ? b - amount : minAmount;
-    }
+	void dim(uint8_t amount, uint8_t minAmount) {
+		r = (r - amount > minAmount) ? r - amount : minAmount;
+		g = (g - amount > minAmount) ? g - amount : minAmount;
+		b = (b - amount > minAmount) ? b - amount : minAmount;
+	}
 
-	_FORCE_INLINE_ bool updated_to_max(const RGBLight& other) {
+	_FORCE_INLINE_ bool updated_to_max(const RGBLight &other) {
 		bool updated = false;
 
 		if (other.r > r) {
@@ -49,27 +49,27 @@ struct alignas(1) RGBLight {
 		return updated;
 	}
 
-	_FORCE_INLINE_ void blend_max(const RGBLight& other) {
+	_FORCE_INLINE_ void blend_max(const RGBLight &other) {
 		r = (r > other.r) ? r : other.r;
 		g = (g > other.g) ? g : other.g;
 		b = (b > other.b) ? b : other.b;
 	}
 
-    _FORCE_INLINE_ uint8_t max_component() const {
-        uint8_t max1 = (r > g) ? r : g;
-        return (max1 > b) ? max1 : b;
-    }
+	_FORCE_INLINE_ uint8_t max_component() const {
+		uint8_t max1 = (r > g) ? r : g;
+		return (max1 > b) ? max1 : b;
+	}
 
 	_FORCE_INLINE_ bool is_lucent() {
 		return (r > 0) || (g > 0) || (b > 0);
 	}
 
-	bool operator==(const RGBLight& other) {
+	bool operator==(const RGBLight &other) {
 		// TODO: Replace this with default spaceship operator (<=>) once we are on C++20
 		return (r == other.r) && (g == other.g) && (b == other.b) && (range == other.range);
 	}
 
-	bool operator!=(const RGBLight& other) {
+	bool operator!=(const RGBLight &other) {
 		// TODO: Replace this with default spaceship operator (<=>) once we are on C++20
 		return (r != other.r) || (g != other.g) || (b != other.b) || (range != other.range);
 	}
@@ -78,26 +78,34 @@ struct alignas(1) RGBLight {
 inline RGBLight sample_point_trilinear(int x, int y, int z, std::array<RGBLight, 20 * 20 * 20> lightData);
 
 inline unsigned int index3D(unsigned int x, unsigned int y, unsigned int z, unsigned int size = 18) {
-    return (x * size + y) * size + z;
+	return (x * size + y) * size + z;
 }
 
-inline void printLights(RGBLight* lights) {
-    // for (int i = 0; i < 18; ++i) {
-    for (int i = 0; i < 1; ++i) {
-        printf("\n");
-        for (int j = 0; j < 18; ++j) {
-            for (int k = 0; k < 18; ++k) {
-                RGBLight value = lights[index3D(j, i, k)];
-                printf("%d,", value.r);
-            }
-            printf("\n");
-        }
-    }
+inline void printLights(RGBLight *lights) {
+	// for (int i = 0; i < 18; ++i) {
+	for (int i = 0; i < 1; ++i) {
+		printf("\n");
+		for (int j = 0; j < 18; ++j) {
+			for (int k = 0; k < 18; ++k) {
+				RGBLight value = lights[index3D(j, i, k)];
+				printf("%d,", value.r);
+			}
+			printf("\n");
+		}
+	}
 }
 
-inline RGBLight sample_lighting_flat(zylann::Vector3f position_world, zylann::Vector3f vertex_position, zylann::Vector3f side_normal, std::array<RGBLight, 20*20*20> lightData, int lightMinimum, bool useShadowTrick, int shadowPenalty) {
-    position_world += side_normal;
-    position_world += zylann::Vector3f(2.0, 2.0, 2.0);
+inline RGBLight sample_lighting_flat(
+		zylann::Vector3f position_world,
+		zylann::Vector3f vertex_position,
+		zylann::Vector3f side_normal,
+		std::array<RGBLight, 20 * 20 * 20> lightData,
+		int lightMinimum,
+		bool useShadowTrick,
+		int shadowPenalty
+) {
+	position_world += side_normal;
+	position_world += zylann::Vector3f(2.0, 2.0, 2.0);
 	float x = position_world.x;
 	float y = position_world.y;
 	float z = position_world.z;
@@ -106,41 +114,41 @@ inline RGBLight sample_lighting_flat(zylann::Vector3f position_world, zylann::Ve
 	int yi = static_cast<int>(y + 0.5);
 	int zi = static_cast<int>(z + 0.5);
 
-    RGBLight lightValue = lightData[index3D(xi, yi, zi, 20)];
+	RGBLight lightValue = lightData[index3D(xi, yi, zi, 20)];
 
-    if (useShadowTrick) {
-        xi = static_cast<int>(x + 0.5 + side_normal.x);
-        yi = static_cast<int>(y + 0.5 + side_normal.y);
-        zi = static_cast<int>(z + 0.5 + side_normal.z);
-        RGBLight secondSample = sample_point_trilinear(xi, yi, zi, lightData);
+	if (useShadowTrick) {
+		xi = static_cast<int>(x + 0.5 + side_normal.x);
+		yi = static_cast<int>(y + 0.5 + side_normal.y);
+		zi = static_cast<int>(z + 0.5 + side_normal.z);
+		RGBLight secondSample = sample_point_trilinear(xi, yi, zi, lightData);
 
-        if (secondSample.r > lightValue.r) {
-            int newValue = static_cast<int>(lightValue.r) - shadowPenalty;
-            newValue = std::max(lightMinimum, newValue);
-            lightValue.r = static_cast<uint8_t>(newValue);
-        }
-        if (secondSample.g > lightValue.g) {
-            int newValue = static_cast<int>(lightValue.g) - shadowPenalty;
-            newValue = std::max(lightMinimum, newValue);
-            lightValue.g = static_cast<uint8_t>(newValue);
-        }
-        if (secondSample.b > lightValue.b) {
-            int newValue = static_cast<int>(lightValue.b) - shadowPenalty;
-            newValue = std::max(lightMinimum, newValue);
-            lightValue.b = static_cast<uint8_t>(newValue);
-        }
-    }
+		if (secondSample.r > lightValue.r) {
+			int newValue = static_cast<int>(lightValue.r) - shadowPenalty;
+			newValue = std::max(lightMinimum, newValue);
+			lightValue.r = static_cast<uint8_t>(newValue);
+		}
+		if (secondSample.g > lightValue.g) {
+			int newValue = static_cast<int>(lightValue.g) - shadowPenalty;
+			newValue = std::max(lightMinimum, newValue);
+			lightValue.g = static_cast<uint8_t>(newValue);
+		}
+		if (secondSample.b > lightValue.b) {
+			int newValue = static_cast<int>(lightValue.b) - shadowPenalty;
+			newValue = std::max(lightMinimum, newValue);
+			lightValue.b = static_cast<uint8_t>(newValue);
+		}
+	}
 
-    return lightValue;
+	return lightValue;
 }
 
-inline RGBLight sample_point_trilinear(int x, int y, int z, std::array<RGBLight, 20*20*20> lightData) {
+inline RGBLight sample_point_trilinear(int x, int y, int z, std::array<RGBLight, 20 * 20 * 20> lightData) {
 	int accumR = 0;
 	int accumG = 0;
 	int accumB = 0;
 	int total_weight = 0;
 
-    // checks 8 neighboring voxels
+	// checks 8 neighboring voxels
 	for (int dz = 0; dz <= 1; ++dz) {
 		for (int dy = 0; dy <= 1; ++dy) {
 			for (int dx = 0; dx <= 1; ++dx) {
@@ -150,7 +158,7 @@ inline RGBLight sample_point_trilinear(int x, int y, int z, std::array<RGBLight,
 
 				RGBLight l = lightData[index3D(nx, ny, nz, 20)];
 
-                if (l.r == 0 && l.g == 0 && l.b == 0) {
+				if (l.r == 0 && l.g == 0 && l.b == 0) {
 					continue;
 				}
 
@@ -166,14 +174,22 @@ inline RGBLight sample_point_trilinear(int x, int y, int z, std::array<RGBLight,
 		uint8_t r = accumR / total_weight;
 		uint8_t g = accumG / total_weight;
 		uint8_t b = accumB / total_weight;
-        return RGBLight{r, g, b};
+		return RGBLight{ r, g, b };
 	}
-	return RGBLight{0, 0, 0};
+	return RGBLight{ 0, 0, 0 };
 }
 
-inline RGBLight sample_lighting(zylann::Vector3f position_world, zylann::Vector3f vertex_position, zylann::Vector3f side_normal, std::array<RGBLight, 20*20*20> lightData, int lightMinimum, bool useShadowTrick, int shadowPenalty) {
-    position_world += vertex_position;
-    position_world += zylann::Vector3f(1.0, 1.0, 1.0);
+inline RGBLight sample_lighting(
+		zylann::Vector3f position_world,
+		zylann::Vector3f vertex_position,
+		zylann::Vector3f side_normal,
+		std::array<RGBLight, 20 * 20 * 20> lightData,
+		int lightMinimum,
+		bool useShadowTrick,
+		int shadowPenalty
+) {
+	position_world += vertex_position;
+	position_world += zylann::Vector3f(1.0, 1.0, 1.0);
 	float x = position_world.x;
 	float y = position_world.y;
 	float z = position_world.z;
@@ -182,32 +198,32 @@ inline RGBLight sample_lighting(zylann::Vector3f position_world, zylann::Vector3
 	int yi = static_cast<int>(y + 0.5);
 	int zi = static_cast<int>(z + 0.5);
 
-    RGBLight lightValue = sample_point_trilinear(xi, yi, zi, lightData);
+	RGBLight lightValue = sample_point_trilinear(xi, yi, zi, lightData);
 
-    if (useShadowTrick) {
-        xi = static_cast<int>(x + 0.5 + side_normal.x);
-        yi = static_cast<int>(y + 0.5 + side_normal.y);
-        zi = static_cast<int>(z + 0.5 + side_normal.z);
-        RGBLight secondSample = sample_point_trilinear(xi, yi, zi, lightData);
+	if (useShadowTrick) {
+		xi = static_cast<int>(x + 0.5 + side_normal.x);
+		yi = static_cast<int>(y + 0.5 + side_normal.y);
+		zi = static_cast<int>(z + 0.5 + side_normal.z);
+		RGBLight secondSample = sample_point_trilinear(xi, yi, zi, lightData);
 
-        if (secondSample.r < lightValue.r) {
-            int newValue = static_cast<int>(lightValue.r) - shadowPenalty;
-            newValue = std::max(lightMinimum, newValue);
-            lightValue.r = static_cast<uint8_t>(newValue);
-        }
-        if (secondSample.g < lightValue.g) {
-            int newValue = static_cast<int>(lightValue.g) - shadowPenalty;
-            newValue = std::max(lightMinimum, newValue);
-            lightValue.g = static_cast<uint8_t>(newValue);
-        }
-        if (secondSample.b < lightValue.b) {
-            int newValue = static_cast<int>(lightValue.b) - shadowPenalty;
-            newValue = std::max(lightMinimum, newValue);
-            lightValue.b = static_cast<uint8_t>(newValue);
-        }
-    }
+		if (secondSample.r < lightValue.r) {
+			int newValue = static_cast<int>(lightValue.r) - shadowPenalty;
+			newValue = std::max(lightMinimum, newValue);
+			lightValue.r = static_cast<uint8_t>(newValue);
+		}
+		if (secondSample.g < lightValue.g) {
+			int newValue = static_cast<int>(lightValue.g) - shadowPenalty;
+			newValue = std::max(lightMinimum, newValue);
+			lightValue.g = static_cast<uint8_t>(newValue);
+		}
+		if (secondSample.b < lightValue.b) {
+			int newValue = static_cast<int>(lightValue.b) - shadowPenalty;
+			newValue = std::max(lightMinimum, newValue);
+			lightValue.b = static_cast<uint8_t>(newValue);
+		}
+	}
 
-    return lightValue;
+	return lightValue;
 }
 
 #endif
